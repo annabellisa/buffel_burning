959e3e71769836a0b5871893500df35d329f43fb
patrickaxe <patrick_di@icloud.com>
2020-04-15 14:50:06 +1000

    outputs update

--- 02_Scripts/Supplement_01_LD_tests.R
+++ 02_Scripts/Supplement_01_LD_tests.R
@@ -18,8 +18,15 @@ invisible(lapply(paste("01_Functions/",dir("01_Functions"),sep=""),function(x) s
 # This is the full data set:
 ghead(snp_onerow); dim(snp_onerow)
 
+----###Running LD Scripts###----
+
 # Perhaps we should use the partially filtered data from STEP 01, which has 29007 loci? This will be quicker than doing the full data set and we are not interested in those loci which have already been filtered out. 
+
+###BD note: Use the partially fitered data from STEP 02, after malim ==0.05 process.
 ghead(filtered_data); dim(filtered_data)
+
+
+----###Warning: Sample dataset were used in following scripts###---- 
 
 # To get the script working, you could run through a smaller data set, that won't take as long to run. Once you have it working you can work on the full filtered_data data set:
 
@@ -67,13 +74,47 @@ df_test<-df_test[which(df_test$r2>0.75),]
 df_test<-tidy.df(df_test)
 
 # write.table(df_test, file="LD_r75_LOCI_FOR_REMOVAL", quote=F, sep="\t", row.names=T)
+write.table(df_test, file="LD_r75_LOCI_FOR_REMOVAL", quote=F, sep="\t", row.names=T)
 
 
 
+----###Warning: full dataset were used in following scripts###---- 
+
+#rand_snp replaced by filtered_data for full data run. 
+sites_to_test<-levels(filtered_data$site)
+dat_test<-filtered_data
+param_file<-paste("RESULTS/LD_results/parameters","_LD.txt",sep="")
+
+#########
+dat_test<-dat_test[,3:ncol(dat_test)]
+
+# geno needs to be m x n where m is the number of markers and n is the number of individuals:
+dat_test<-t(dat_test)
+
+ghead(dat_test); dim(dat_test)
+
+ld.thisrun<-calc_LD(dat_test,inds=1:nrow(dat_test), get.D=F, get.Dprime=F, get.rsq=T, get.chisq=F, get.chisq_prime=F)
+
+loc_combn<-combn(rownames(dat_test),2)
+
+df_test<-data.frame(loc1=loc_combn[1,],loc2=loc_combn[2,],r2=ld.thisrun$rsq[lower.tri(ld.thisrun$rsq)])
+
+# for all loci, there are 182739403 pairwise comparisons of linkage disequilibrium:
+head(df_test); dim(df_test)
+
+hist(df_test$r2)
 
 
 
+# we need to decide what a reasonable cut-off is for discarding linked loci. In the PNAS paper, we used 0.75
+
+# the STEP_02 script pulls in a file of linked loci. If the file is very large, it might be good to reduce it to only those locus pairs above the cut-off:
+df_test<-df_test[which(df_test$r2>0.75),]
+df_test<-tidy.df(df_test)
+
+############
 
 
 
+write.table(df_test, file="LD_r75_LOCI_FOR_REMOVAL", quote=F, sep="\t", row.names=T)
 
--- 02_Scripts/main_script.R
+++ 02_Scripts/main_script.R
@@ -63,53 +63,33 @@ ghead(filtered_data); dim(filtered_data)
 ls(); View(filtered_data)
 str(filtered_data)
 
-#----------Script 14/04/2020---------#
-#cr<-0.5 @#42
+#----------Script 15/04/2020---------#
+#> ghead(filtered_data); dim(filtered_data)
+#site     ind L6 L7 L8 L9 L12 L26 L37 L47
+#1 X01b X01b_01  2  1  2  0   0   0   2   1
+#2 X01b X01b_02  2  1  2  0   0   0   2   1
+#3 X01b X01b_03  2  1  2  0   0   0   2   1
+#4 X01b X01b_04  2  1  2  0   0   0   2   1
+#5 X01u X01u_01  1  2  0  0   0   0   0   1
+#6 X01u X01u_02  2  1  2  0   0   0   2   1
+#[1]    93 19120
 
-#quality rate: 0.05 from 
-#"C:/Users/s4467005/OneDrive - The University of Queensland/Smith Lab/Schilling_etal_2014_PlosOne_HWE.pdf"
-#Call rate 0.8-0.9:0.8/0.85/0.9
-#---0.9-0.8:
-#"There were no loci with more than 90 % missing data; none removed"
-#0.5:"6164 loci with more than 50 % missing data removed"
+#> ghead(dat_test); dim(dat_test)
+#1 2 3 4 5 6 7 8 9 10
+#L6  2 2 2 2 1 2 2 2 2  2
+#L7  1 1 1 1 2 1 1 1 1  1
+#L8  2 2 2 2 0 2 2 2 2  2
+#L9  0 0 0 0 0 0 0 0 0  0
+#L12 0 0 0 0 0 0 0 0 0  0
+#L26 0 0 0 0 0 0 0 0 0  0
+#[1] 19118    93
 
-
-# Remarks derived from filter_SNPs:
-# Cannot use DartSeq CallRate filter because it was calculated on the whole dataset. Individuals from the RCH and FS populations will cause higher proportion of missing data in the rest of the data:
-# This metric is highly correlated with the DartSeq metric (99%), but it filters loci biased by the presence of other populations, e.g. removing the firescape samples results in approx. 500 extra loci filtered.
-# cr<-linf[,c("locus","CallRate")]
-# miss<-merge(miss,cr,by="locus",all.x=T,all.y=F)
-# plot(miss$missing_data,miss$CallRate)
-# cor.test(miss$missing_data,miss$CallRate)
-# callrate50<-miss$locus[which(miss$missing_data>0.5)]
-# callrate50_dart<-miss$locus[which(linf$CallRate<0.5)]
-
-
-#ra<-0.98 @#50
-#0.95 instead? 
-
-
-#malim<-0.01 @#65
-#0.01*1 v 0.05*2--> 0.05 instead?
-
-
-#> LD_dir<-"../../ANALYSIS_RESULTS/LINKAGE_DISEQUILIBRIUM/LD_parameters"
-#> dir(LD_dir)
-#character(0)????
-#Need a file: LD_r75_over5pop_LOCI_FOR_REMOVAL.txt
-
-
-
-#Not Sure:
-# Filter loci with were in LD in > 5 populations with a correlation of 0.75 (see Supplement_03_LD_tests.R for details):
-#@73 Error
-#> LD_dir<-"../../ANALYSIS_RESULTS/LINKAGE_DISEQUILIBRIUM/LD_parameters"
-#> dir(LD_dir)
-#character(0)
-#> ld_loc<-read.table(paste(LD_dir, "LD_r75_over5pop_LOCI_FOR_REMOVAL.txt",sep="/"),header=T)
-#Error in file(file, "rt") : cannot open the connection
-#In addition: Warning message:
-  #In file(file, "rt") :
-  #cannot open file '../../ANALYSIS_RESULTS/LINKAGE_DISEQUILIBRIUM/LD_parameters/LD_r75_over5pop_LOCI_FOR_REMOVAL.txt': No such file or directory
-
-
+#> head(df_test); dim(df_test)
+#loc1 loc2           r2
+#1   L6   L7 0.0001216915
+#2   L6   L8 0.4972949160
+#3   L6   L9 0.2656598485
+#4   L6  L12 0.6284829721
+#5   L6  L26 0.6263439360
+#6   L6  L37 0.6318159116
+#[1] 182739403         3
